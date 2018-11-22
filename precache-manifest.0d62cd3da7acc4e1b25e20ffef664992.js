f29578235fb92411f22986a64ac80161bcbd39aa
samson <nguyenhuykaio@gmail.com>
2018-11-22 22:32:01 +0700

    Updates

---
+++ precache-manifest.0d62cd3da7acc4e1b25e20ffef664992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3254cae39f4665671f11",
+    "url": "/testpv/static/js/runtime~main.3254cae3.js"
+  },
+  {
+    "revision": "afcb1cb24f3f11bf181f",
+    "url": "/testpv/static/js/main.afcb1cb2.chunk.js"
+  },
+  {
+    "revision": "5dab2ea65888ee2c5e5d",
+    "url": "/testpv/static/js/1.5dab2ea6.chunk.js"
+  },
+  {
+    "revision": "afcb1cb24f3f11bf181f",
+    "url": "/testpv/static/css/main.bfb04f46.chunk.css"
+  },
+  {
+    "revision": "6389af420903d3e9eabc576f7222faf0",
+    "url": "/testpv/index.html"
+  }
+];
